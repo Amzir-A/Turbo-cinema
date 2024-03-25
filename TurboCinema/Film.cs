d587ccae3b0161c84374c0cfa19b2b1aae354299
SvenvanMuiden <Svenvm2004@gmail.com>
2024-03-25 12:40:54 +0100

    e

---
+++ TurboCinema/Film.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+public class Film
+{
+    public string Title { get; set; }
+    public string Release { get; set; }
+    public string Director { get; set; } 
+    public List<string> Actors { get; set; }
+    public string Duration { get; set; }
+    public List<string> Genre { get; set; } 
+    public string AgeRating { get; set; } 
+    public string Description { get; set; }
+
+    public Film(string title, string release, string director, List<string> actors, string duration, List<string> genre, string ageRating, string description)
+    {
+        Title = title;
+        Release = release;
+        Director = director;
+        Actors = actors ?? new List<string>();
+        Duration = duration;
+        Genre = genre ?? new List<string>();
+        AgeRating = ageRating;
+        Description = description;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} ({Release}) - {Director}";
+    }
+}
