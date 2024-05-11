90e0d7a3ef67ff6347fa64cde13235464a22ac9c
mayrii101 <161327932+mayrii101@users.noreply.github.com>
2024-05-11 16:29:23 +0200

    Update Customer.cs

--- TurboCinema/Customer.cs
+++ TurboCinema/Customer.cs
@@ -1,12 +1,12 @@
 public class Customer
 {
-    public int CustomerId { get; set; }
+    public int ID { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string DateOfBirth { get; set; }
-    public string Password { get; set; }
+    public DateTime DateOfBirth { get; set; }
     public string Email { get; set; }
     public string Postcode { get; set; }
+    public string Password { get; set; }
     public List<Reservation> Reservations { get; set; }
 
     public Customer()
