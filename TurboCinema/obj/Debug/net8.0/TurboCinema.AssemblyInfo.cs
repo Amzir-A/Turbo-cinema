0ae74b05c1c276de2cac5e255808949c3cc2aeff
SvenvanMuiden <Svenvm2004@gmail.com>
2024-05-22 15:41:19 +0200

    ;

--- TurboCinema/obj/Debug/net8.0/TurboCinema.AssemblyInfo.cs
+++ TurboCinema/obj/Debug/net8.0/TurboCinema.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurboCinema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96394d46dd9491582e63c9ee438a3a905a24e060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b519ecae220862ed72d8536121b76b38df4796c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurboCinema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurboCinema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
