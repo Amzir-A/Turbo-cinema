a2a798dc49b8a58736c0bb01e6c0dd665f0b5371
Nolanimations <73882894+nolanimations@users.noreply.github.com>
2024-06-05 14:30:39 +0200

    eten en drinken buttons

--- TurboCinema/obj/Debug/net8.0/TurboCinema.AssemblyInfo.cs
+++ TurboCinema/obj/Debug/net8.0/TurboCinema.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurboCinema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f83e079e14a6f5a747f103f2b4f35a3ebf9dae6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+249d95e6994099a2504dc56051e5441639d5a612")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurboCinema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurboCinema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
