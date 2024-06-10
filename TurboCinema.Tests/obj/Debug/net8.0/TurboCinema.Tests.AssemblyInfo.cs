be24d2f2e4faba57f90d5e04a9f93313bef816c2
Amzir-A <102997068+Amzir-A@users.noreply.github.com>
2024-06-10 11:13:29 +0200

    x

--- TurboCinema.Tests/obj/Debug/net8.0/TurboCinema.Tests.AssemblyInfo.cs
+++ TurboCinema.Tests/obj/Debug/net8.0/TurboCinema.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurboCinema.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c686ae54a0fac33a90a3d9c857cdacf34dfb273c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586ca05c154042dcb7ba12a01a6262d6c9e25690")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurboCinema.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurboCinema.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
