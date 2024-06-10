45f9f83ccf4711bf217366e1cde4b74b8f306548
SvenvanMuiden <Svenvm2004@gmail.com>
2024-06-10 10:25:13 +0200

    a

--- TurboCinema.Tests/obj/Debug/net8.0/TurboCinema.Tests.AssemblyInfo.cs
+++ TurboCinema.Tests/obj/Debug/net8.0/TurboCinema.Tests.AssemblyInfo.cs
@@ -13,15 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurboCinema.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d295b43b122ff6cda12234484d232349a7eb5cac")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ad81951d22c48e4ea789ed6cbbf87150573aad")]
->>>>>>> Svennerz
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41957526cd22d5a8cd3f062e2e09a70ec04673a")]
->>>>>>> Svennerz
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec72daafeb8ba3c3014f3022efeb39511485cbe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurboCinema.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurboCinema.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
