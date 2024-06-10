009e00b190a2ae33d82d0265cf527772d97d473c
Nolanimations <73882894+nolanimations@users.noreply.github.com>
2024-06-10 13:27:56 +0200

    betere email check

--- TurboCinema/obj/Debug/net8.0/TurboCinema.AssemblyInfo.cs
+++ TurboCinema/obj/Debug/net8.0/TurboCinema.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurboCinema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69e245a2ecfcfb5851f523fdd9673a65f9c8068")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9eaa0f1cf2cf1edb466b14fcd45f2fbc474cf07")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurboCinema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurboCinema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
